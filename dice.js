a79f77e06764b723e01593d5295bde6dd03bc1b2
Ali-Assaad <aliassaadtr@gmail.com>
2022-05-23 10:47:51 +0300

    Add generic functions

--- dice.js
+++ dice.js
@@ -1,7 +1,25 @@
 /********************* Functions /**********************/
+function getRandomNumber(){
+    let ran = Math.floor(Math.random() * 6) + 1
+}
+function throwFirstDice(){
+
+    
+}
+
+function throwSecondDice(){
+    
+}
+
+function displayResult(){
+    
+}
+
+/********************* Initliaze dice game /**********************/
+
 function intiDice(){
 
-    displayStartGameMessage()
+    // displayStartGameMessage()
     throwFirstDice()
     throwSecondDice()
     displayResult()
